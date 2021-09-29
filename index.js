e63a037281852b2121f1b0969873c59364ba6556
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-09-29 17:20:49 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const friends=["Wes","Clayton","Katerina","Tenzin"]
+
+function writeCards(names,event){
+    let thankYouList=[]
+    for (let counter=0;counter<(names.length);counter++){
+        thankYouList.push(`Thank you, ${names[counter]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouList
+}
+
+console.log(writeCards(friends,"birthday"))
+
+function countDown(x){
+    let counter=x
+    while(counter>=0){
+        console.log(counter)
+        counter--
+    }
+}
